5ceac4414e740a95546971953661e30f586b9ff1
Evan Raskob <info@pixelist.info>
2018-09-27 14:23:51 -0400

    fixed circle funtion but needs testing

--- liveprinter/static/examples/circle.js
+++ liveprinter/static/examples/circle.js
@@ -21,21 +21,51 @@ lp.moveto({ x: lp.cx, y: lp.cy, z: lp.layerHeight });
 
 // define a circle function -- the lp.circle is a bit like this
 function circle (r, cx, cy, segs=10) {
-    for (let i = 0; i < segs; i++) {
-        let angle = Math.PI * 2 * i / (segs-1);
-        let x = Math.cos(angle) * r;
-        let y = Math.sin(angle) * r;
-        lp.extrudeto({ 'x': cx + x, 'y': cy + y});
+
+    // law of cosines
+    const r2x2 = r * r * 2;
+    const x = sqrt(r2x2 - r2x2 * cos(segAngle));
+    const segAngle = Math.PI * 2 / segs;
+
+    //translate((float)radius, 0);
+    lp.turn(Math.PI / 2);
+    // we're in the middle of segment
+    lp.turn(-segAngle / 2);
+    
+    for (let i = 0; i < segs; i++)
+    {
+        lp.turn(segAngle);
+        // print without retraction
+        lp.dist(x).go(1, false);
     }
 }
+
+
+// another way
+let segs = 10;
+for (let i = 0; i < segs; i++) {
+    let angle = Math.PI * 2 * i / (segs - 1);
+    let x = Math.cos(angle) * r;
+    let y = Math.sin(angle) * r;
+    lp.extrudeto({ 'x': cx + x, 'y': cy + y });
+}
+
 
 // make a cylinder by moving up each time
 lp.circle(30, lp.cx, lp.cy);
 lp.move({ z: lp.layerHeight });
 
 
-// draw a "cone"
+// draw a "cone" thing
+let r = 30;
+let offset = 0.2;
+
 for (let i = 0; i < 20; i++) {
-    lp.circle(30 - i / 20, lp.cx, lp.cy);
-    lp.move({ z: lp.layerHeight })
+    lp.circle(r).retract();
+
+    lp.turn(90).dist(offset).go(); // move towards centre slightly
+    lp.move({ z: lp.layerHeight }); // move up a level
+    lp.turn(-90);
+    r -= offset;
+    lp.unretract();
 }
