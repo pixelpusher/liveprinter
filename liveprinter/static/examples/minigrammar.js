3eefe63bba33e1d3f92aa959953c87158284f436
Evan Raskob <info@pixelist.info>
2019-07-26 16:40:34 +0100

    fixed errors in minigrammar example

--- liveprinter/static/examples/minigrammar.js
+++ liveprinter/static/examples/minigrammar.js
@@ -15,17 +15,17 @@ lp.moveto({ x: 20, y: 20, speed: 80 });
 
 // You can use the '|' character to chain together functions like so:
 
-# lp.start 210 | move x: 23 y: 50 z: 10 | turn 23 | go 1
+# lp.start 210 | move x: 23 y: 50 z: 10 | extrude x:50 speed:15
 
 // compiles to: lp.start(210).move({x:23,y:50,z:10}).turn(23).go(1);
 
 // you can also do blocks:
 
 ##
-lp.moveto x: 20 y: 30 speed: 40 | lp.extrude e: 10 speed: 8
-
+lp.moveto x: 20 y: 30 speed: 40
+lp.extrude e: 10 speed: 8
 lp.start 210
 
-lp.m2s 64 | lp.go 1
+lp.m2s 64 | t2d 400 | go 1
 
 ##
