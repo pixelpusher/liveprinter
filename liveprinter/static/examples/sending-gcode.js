ca63dc82e3013396ee1490d3accd9f62798c9848
Evan Raskob <info@pixelist.info>
2020-04-28 11:04:53 +0100

    committed missing files from vs code project and slight examples update

--- liveprinter/static/examples/sending-gcode.js
+++ liveprinter/static/examples/sending-gcode.js
@@ -4,37 +4,39 @@
 
 // home all axes - this is useful to make sure the print head position is accurate
 // this is also run as part of lp.start()
-gcode("G28");
+await gcode("G28");
+
+# send "G28"
 
 //get the current positions of all the motors (x,y,z,e)
 // will display in the Info panel
-gcode("M114");
+await gcode("M114");
 
 //turn on hot end to 190C
-gcode("M104 S190");
+await gcode("M104 S190");
 
 //heat bed to 60C
-gcode("M140 S60");
+await gcode("M140 S60");
 
 //heat bed to 60C and wait until fully heated to proceed
-gcode("M190 S60");
+await gcode("M190 S60");
 
 // get the current temperature (will show in the lower right side box)
 // this is what happens when we poll the temperature
-gcode("M119");
+await gcode("M119");
 
-gcode("M104 S0"); // turn off the heater for the hot end
-gcode("M106 S100"); // turn fan on to full (100)
+await gcode("M104 S0"); // turn off the heater for the hot end
+await gcode("M106 S100"); // turn fan on to full (100)
 
 //have the printer wait until the temperature is reached
-gcode("M109 S200"); // turn on the heater for the hot end to 200C and wait until temperature is reached to proceeed
+await gcode("M109 S200"); // turn on the heater for the hot end to 200C and wait until temperature is reached to proceeed
 
 // turn off fan
-gcode("M107");
+await gcode("M107");
 
 // BEEPING!
 // beep at 440Hz for 2 seconds (2000ms)
-gcode("M300 S440 P2000");
+await gcode("M300 S440 P2000");
 
 
 // RETRACTION:
